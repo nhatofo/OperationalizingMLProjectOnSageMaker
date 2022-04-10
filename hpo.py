0740b316ee9b780961b9119abefcfe7f56ba9090
Teofilo Chichume <nhatofo@gmail.com>
2022-04-11 00:08:34 +0200

    Another commit before submit

--- hpo.py
+++ hpo.py
@@ -1,15 +1,11 @@
 
 import numpy as np
 import torch
-import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
-import torch.utils.data
-import torch.utils.data.distributed
 import torchvision
 import torchvision.models as models
 import torchvision.transforms as transforms
-import json
 
 import copy
 import argparse
@@ -42,14 +38,7 @@ def test(model, test_loader, criterion):
     logger.info(f"Testing Loss: {total_loss}")
     logger.info(f"Testing Accuracy: {total_acc}")
 
-def _average_gradients(model):
-    # Gradient averaging.
-    size = float(dist.get_world_size())
-    for param in model.parameters():
-        dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM)
-        param.grad.data /= size
-    
-def train(model, train_loader, validation_loader, criterion, optimizer, is_distributed):
+def train(model, train_loader, validation_loader, criterion, optimizer):
     epochs=50
     best_loss=1e6
     image_dataset={'train':train_loader, 'valid':validation_loader}
@@ -72,9 +61,6 @@ def train(model, train_loader, validation_loader, criterion, optimizer, is_distr
                 if phase=='train':
                     optimizer.zero_grad()
                     loss.backward()
-                    #if is_distributed:
-                        # average gradients manually for multi-machine cpu case only
-                    #    _average_gradients(model)
                     optimizer.step()
 
                 _, preds = torch.max(outputs, 1)
@@ -113,7 +99,7 @@ def net():
                    nn.Linear(128, 133))
     return model
 
-def create_data_loaders(data, batch_size, is_distributed):
+def create_data_loaders(data, batch_size):
     train_data_path = os.path.join(data, 'train')
     test_data_path = os.path.join(data, 'test')
     validation_data_path=os.path.join(data, 'valid')
@@ -130,55 +116,28 @@ def create_data_loaders(data, batch_size, is_distributed):
         ])
 
     train_data = torchvision.datasets.ImageFolder(root=train_data_path, transform=train_transform)
-    train_sampler = (
-        torch.utils.data.distributed.DistributedSampler(train_data) if is_distributed else None
-    )
-    train_data_loader = torch.utils.data.DataLoader(
-        train_data,
-        batch_size=batch_size,
-        shuffle=train_sampler is None,
-        sampler=train_sampler,
-    )
+    train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
     test_data = torchvision.datasets.ImageFolder(root=test_data_path, transform=test_transform)
-    test_data_loader  = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False)
+    test_data_loader  = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
     validation_data = torchvision.datasets.ImageFolder(root=validation_data_path, transform=test_transform)
-    validation_data_loader  = torch.utils.data.DataLoader(validation_data, batch_size=batch_size, shuffle=False) 
+    validation_data_loader  = torch.utils.data.DataLoader(validation_data, batch_size=batch_size, shuffle=True) 
     
     return train_data_loader, test_data_loader, validation_data_loader
 
 def main(args):
     logger.info(f'Hyperparameters are LR: {args.learning_rate}, Batch Size: {args.batch_size}')
     logger.info(f'Data Paths: {args.data}')
-
-    is_distributed = len(args.hosts) > 1 and args.backend is not None
-    logger.debug("Distributed training - {}".format(is_distributed))
     
-    if is_distributed:
-        # Initialize the distributed environment.
-        world_size = len(args.hosts)
-        os.environ["WORLD_SIZE"] = str(world_size)
-        host_rank = args.hosts.index(args.current_host)
-        os.environ["RANK"] = str(host_rank)
-        dist.init_process_group(backend=args.backend, rank=host_rank, world_size=world_size)
-        logger.info(
-            "Initialized the distributed environment: '{}' backend on {} nodes. ".format(
-                args.backend, dist.get_world_size()
-            )
-            + "Current host rank is {}".format(dist.get_rank())
-        )
-    
-    train_loader, test_loader, validation_loader=create_data_loaders(args.data, args.batch_size, is_distributed)
+    train_loader, test_loader, validation_loader=create_data_loaders(args.data, args.batch_size)
     model=net()
     
     criterion = nn.CrossEntropyLoss(ignore_index=133)
     optimizer = optim.Adam(model.fc.parameters(), lr=args.learning_rate)
-
-    model = torch.nn.DataParallel(model)
     
     logger.info("Starting Model Training")
-    model=train(model, train_loader, validation_loader, criterion, optimizer, is_distributed)
+    model=train(model, train_loader, validation_loader, criterion, optimizer)
     
     logger.info("Testing Model")
     test(model, test_loader, criterion)
@@ -193,15 +152,6 @@ if __name__=='__main__':
     parser.add_argument('--data', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
     parser.add_argument('--model_dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--output_dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
-    parser.add_argument("--hosts", type=list, default=json.loads(os.environ["SM_HOSTS"]))
-    parser.add_argument("--current-host", type=str, default=os.environ["SM_CURRENT_HOST"])
-    
-    parser.add_argument(
-        "--backend",
-        type=str,
-        default=None,
-        help="backend for distributed training (tcp, gloo on cpu and gloo, nccl on gpu)",
-    )
     
     args=parser.parse_args()
     print(args)
